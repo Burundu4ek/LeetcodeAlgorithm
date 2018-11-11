a48e38250f17de38c10181203538aed7fb5a1ba9
natali <nataliakuznetc@gmail.com>
2018-11-10 20:14:42 -0800

    isPalindrome solution

---
+++ src/main/java/palindromeNumber/Solution.java
@@ -0,0 +1,15 @@
+package palindromeNumber;
+
+public class Solution {
+    public boolean isPalindrome(int x) {
+        char[] chars = String.valueOf(x).toCharArray();
+        int l = 0, h = chars.length - 1;
+
+        while(l < h) {
+            if (chars[l++] != chars[h--])
+                return false;
+        }
+
+        return true;
+    }
+}
