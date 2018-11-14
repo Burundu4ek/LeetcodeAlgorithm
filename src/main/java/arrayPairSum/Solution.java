e8cf92ece184e289682262f71f93f968e6fc1253
natali <nataliakuznetc@gmail.com>
2018-11-14 14:21:51 -0800

    arrayPairSum solution

---
+++ src/main/java/arrayPairSum/Solution.java
@@ -0,0 +1,42 @@
+package arrayPairSum;
+
+public class Solution {
+    public int arrayPairSum(int[] nums) {
+        quickSort(nums, 0, nums.length - 1);
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i += 2) {
+            sum += nums[i];
+        }
+
+        return sum;
+    }
+
+    private int partition(int arr[], int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1);
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                int temp = arr[++i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    private void quickSort(int arr[], int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+}
