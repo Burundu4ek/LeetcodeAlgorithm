8646a420c95b3a07caf258c3ca0b145a65139dc1
natali <nataliakuznetc@gmail.com>
2018-11-14 13:41:45 -0800

    reverseString solution

---
+++ src/main/java/reverseString/Solution.java
@@ -0,0 +1,16 @@
+package reverseString;
+
+public class Solution {
+    public String reverseString(String s) {
+        char[] res = s.toCharArray();
+        int i = 0, j = s.length() - 1;
+
+        while (i < j) {
+            char t = res[i];
+            res[i++] = res[j];
+            res[j--] = t;
+        }
+
+        return String.valueOf(res);
+    }
+}
