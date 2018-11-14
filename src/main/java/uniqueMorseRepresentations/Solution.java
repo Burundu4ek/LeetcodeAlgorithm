3e08ba442ccd42955257c9d3f38d34911f1645d1
natali <nataliakuznetc@gmail.com>
2018-11-14 14:59:57 -0800

    uniqueMorseRepresentations solution

---
+++ src/main/java/uniqueMorseRepresentations/Solution.java
@@ -0,0 +1,26 @@
+package uniqueMorseRepresentations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        if (words.length == 0)
+            return 0;
+
+        Set<String> res = new HashSet<String>();
+        String[] codes = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        for (String w : words) {
+            StringBuilder str = new StringBuilder();
+            for (char c : w.toCharArray()) {
+                int index = alphabet.indexOf(c);
+                str.append(codes[index]);
+            }
+            res.add(str.toString());
+        }
+
+        return res.size();
+    }
+}
