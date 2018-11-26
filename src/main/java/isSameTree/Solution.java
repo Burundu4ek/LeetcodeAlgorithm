1df2e70752e23ea09f146554f64f2fe0cad969b0
natali <nataliakuznetc@gmail.com>
2018-11-25 21:43:11 -0800

    isSameTree solution with preorderTraversal

---
+++ src/main/java/isSameTree/Solution.java
@@ -0,0 +1,38 @@
+package isSameTree;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    boolean res = true;
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        res = compTraversal(p, q);
+        return res;
+    }
+
+    private boolean compTraversal(TreeNode rF, TreeNode rS){
+        if (!(rF == null && rS == null)) {
+            if (rF == null || rS == null){
+                res = false;
+                return false;
+            }
+
+            if (rF.val != rS.val){
+                res = false;
+                return false;
+            }
+
+            compTraversal(rF.left, rS.left);
+            compTraversal(rF.right, rS.right);
+        }
+
+        return res;
+    }
+}
