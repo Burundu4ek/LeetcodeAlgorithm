2a558d0b83f53562a95d9ef3f4a052fdff484caf
natali <nataliakuznetc@gmail.com>
2018-11-26 22:01:16 -0800

    removeDuplicates solution

---
+++ src/main/java/removeDuplicatesFromSortedArray/Solution.java
@@ -0,0 +1,13 @@
+package removeDuplicatesFromSortedArray;
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int length = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[length] < nums[i])
+                nums[++length] = nums[i];
+        }
+
+        return ++length;
+    }
+}
