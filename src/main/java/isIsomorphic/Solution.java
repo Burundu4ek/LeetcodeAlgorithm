ef69623afca87a0cee4e5da6db749bb9ede75089
natali <nataliakuznetc@gmail.com>
2019-02-26 16:44:54 -0800

    isIsomorphic solution

---
+++ src/main/java/isIsomorphic/Solution.java
@@ -0,0 +1,22 @@
+package isIsomorphic;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        Map<Character, Character> charMap = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (charMap.containsKey(s.charAt(i))) {
+                if (!charMap.get(s.charAt(i)).equals(t.charAt(i)))
+                    return false;
+            } else if (charMap.containsValue(t.charAt(i))) {
+                return false;
+            } else
+                charMap.put(s.charAt(i), t.charAt(i));
+        }
+
+        return true;
+    }
+}
