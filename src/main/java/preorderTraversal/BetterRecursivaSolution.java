0ab424243f6548272566ff35ced659c7464b414c
natali <nataliakuznetc@gmail.com>
2019-03-06 10:24:31 -0800

    BetterRecursiveSolution solution

---
+++ src/main/java/preorderTraversal/BetterRecursivaSolution.java
@@ -0,0 +1,18 @@
+package preorderTraversal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BetterRecursivaSolution {
+    List<Integer> res = new ArrayList<>();
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if (root == null) return res;
+
+        res.add(root.val);
+        preorderTraversal(root.left);
+        preorderTraversal(root.right);
+
+        return res;
+    }
+}
