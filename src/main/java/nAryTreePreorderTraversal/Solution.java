3e295e4e313caa27593af629a556f057c00b3b87
natali <nataliakuznetc@gmail.com>
2019-03-06 10:34:29 -0800

    N-Ary Tree Preorder traversal solution

---
+++ src/main/java/nAryTreePreorderTraversal/Solution.java
@@ -0,0 +1,19 @@
+package nAryTreePreorderTraversal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    List<Integer> res = new ArrayList<>();
+
+    public List<Integer> preorder(Node root) {
+        if (root == null) return res;
+
+        res.add(root.val);
+        for (Node child : root.children) {
+            preorder(child);
+        }
+
+        return res;
+    }
+}
