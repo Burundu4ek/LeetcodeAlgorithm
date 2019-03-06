a094efd5db02838dc0b45abef0520c969f421196
natali <nataliakuznetc@gmail.com>
2019-03-06 10:25:09 -0800

    fix solution name

--- src/main/java/preorderTraversal/BetterRecursiveSolution.java
+++ src/main/java/preorderTraversal/BetterRecursiveSolution.java
@@ -3,7 +3,7 @@ package preorderTraversal;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BetterRecursivaSolution {
+public class BetterRecursiveSolution {
     List<Integer> res = new ArrayList<>();
 
     public List<Integer> preorderTraversal(TreeNode root) {
